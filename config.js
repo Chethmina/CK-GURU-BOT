6ca69c4d36b1cd425791758973ed719d2f99941d
Chethmina <106663853+Chethmina@users.noreply.github.com>
2023-05-14 12:28:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94722770763', '𝐂𝐡𝐞𝐭𝐡𝐦𝐢𝐧𝐚', true],
+  ['94722770762', '𝐂𝐡𝐞𝐭𝐡𝐦𝐢𝐧𝐚', true], 
   [''] 
 ] //Number of owners
 
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝐂𝐊 𝐆𝐔𝐑𝐔 𝐁𝐨𝐭'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '𝙲𝙺 𝙶𝚄𝚁𝚄' 
+global.author = '2023' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
